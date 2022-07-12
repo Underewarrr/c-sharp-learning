023320a208d399528d58416ba7f72388b8707d39
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-12 12:46:40 -0300

    Dev: Learning

--- Course-Csharp/Program.cs
+++ Course-Csharp/Program.cs
@@ -1,23 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// Tipos
+﻿// Tipos
 // int, float, double, string, bool, char, byte, short, long, decimal
 
 int b, b2;
+b = 10;
+b2 = b;
 
 char c, c2;
+c = 'a';
+c2 = c;
 
 byte b3, b4;
 
+b3 = 10;
+b4 = b3;
+
 int i, i2;
+
+i = 10;
+i2 = i;
 
 long l, l2;
 
+l = 10;
+l2 = l;
+
 float f, f2;
+
+f = 10.0f;
+f2 = f;
 
 double d, d2;
 
 decimal d3, d4;
 
 string s, s2;
+
+Console.WriteLine("Valor de b e b2 " + b + " " + b2);
+Console.WriteLine("valor de c e c2 " + c + " " + c2);
+Console.WriteLine("valor de b3 e b4 " + b3 + " " + b4);
+Console.WriteLine("valor de i e i2 " + i + " " + i2);
+Console.WriteLine("valor de l e l2 " + l + " " + l2);
+Console.WriteLine("valor de f e f2 " + f + " " + f2);
