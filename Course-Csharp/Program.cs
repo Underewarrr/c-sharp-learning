81a01ca4bf992a948ee254a462b14534afd4487a
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-12 12:51:29 -0300

    Dev: Tipos primitivos c#

--- Course-Csharp/Program.cs
+++ Course-Csharp/Program.cs
@@ -27,13 +27,25 @@ l2 = l;
 float f, f2;
 
 f = 10.0f;
-f2 = f;
+f2 = f + 1.0f;
 
 double d, d2;
 
+d = 10.0;
+d2 = d + 1.0;
+
+d = 10.0d;
+d2 = d + 10.1d;
+
 decimal d3, d4;
 
+d3 = 10.0m;
+d4 = d3 + 1.0m;
+
 string s, s2;
+
+s = "Hello Worl";
+s2 = s + 'd';
 
 Console.WriteLine("Valor de b e b2 " + b + " " + b2);
 Console.WriteLine("valor de c e c2 " + c + " " + c2);
@@ -41,3 +53,5 @@ Console.WriteLine("valor de b3 e b4 " + b3 + " " + b4);
 Console.WriteLine("valor de i e i2 " + i + " " + i2);
 Console.WriteLine("valor de l e l2 " + l + " " + l2);
 Console.WriteLine("valor de f e f2 " + f + " " + f2);
+Console.WriteLine("valor de d e d2 " + d + " " + d2);
+Console.WriteLine("valor de d3 e d4 " + d3 + " " + d4);
