cfc0ade1d3d4edbb6f8c82cab91363e47abc4ba7
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-12 13:35:24 -0300

    Dev: Top level statement

--- Course-Csharp/Program.cs
+++ Course-Csharp/Program.cs
@@ -1,32 +1,15 @@
 ﻿// Tipos
 // int, float, double, string, bool, char, byte, short, long, decimal
 
-Console.Write("Digite Uma String: ");
-
-string csharp = Console.ReadLine();
-
-Console.WriteLine($"Sua String {csharp}. Informações: ");
-
-Console.WriteLine($"Possui {csharp.Length} caracteres.");
-Console.WriteLine($"O Primeiro Caracter É {csharp[0]}.");
-Console.WriteLine($"O Último Caracter É {csharp[csharp.Length - 1]}.");
-
-if (csharp.Length % 2 == 0)
+class Prgram 
 {
-    Console.WriteLine("O Número É PAR.");
-}
-else
-{
-    Console.WriteLine("O Número É IMPAR.");
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        Console.WriteLine(c);
+    }
 }
 
-if (csharp.Length % 3 == 0)
-{
-    Console.WriteLine("O Número É MÚLTIPLO DE 3.");
-}
-else
-{
-    Console.WriteLine("O Número NÃO É MÚLTIPLO DE 3.");
-}
-
-
+string [] nomes = { "João", "Maria", "José" };
