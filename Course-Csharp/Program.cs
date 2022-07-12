ba9452f04d883411a207028f34295a983b80b42a
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-12 13:15:47 -0300

    Dev: Console app

--- Course-Csharp/Program.cs
+++ Course-Csharp/Program.cs
@@ -1,57 +1,32 @@
 ﻿// Tipos
 // int, float, double, string, bool, char, byte, short, long, decimal
 
-int b, b2;
-b = 10;
-b2 = b;
+Console.Write("Digite Uma String: ");
 
-char c, c2;
-c = 'a';
-c2 = c;
+string csharp = Console.ReadLine();
 
-byte b3, b4;
+Console.WriteLine($"Sua String {csharp}. Informações: ");
 
-b3 = 10;
-b4 = b3;
+Console.WriteLine($"Possui {csharp.Length} caracteres.");
+Console.WriteLine($"O Primeiro Caracter É {csharp[0]}.");
+Console.WriteLine($"O Último Caracter É {csharp[csharp.Length - 1]}.");
 
-int i, i2;
+if (csharp.Length % 2 == 0)
+{
+    Console.WriteLine("O Número É PAR.");
+}
+else
+{
+    Console.WriteLine("O Número É IMPAR.");
+}
 
-i = 10;
-i2 = i;
+if (csharp.Length % 3 == 0)
+{
+    Console.WriteLine("O Número É MÚLTIPLO DE 3.");
+}
+else
+{
+    Console.WriteLine("O Número NÃO É MÚLTIPLO DE 3.");
+}
 
-long l, l2;
 
-l = 10;
-l2 = l;
-
-float f, f2;
-
-f = 10.0f;
-f2 = f + 1.0f;
-
-double d, d2;
-
-d = 10.0;
-d2 = d + 1.0;
-
-d = 10.0d;
-d2 = d + 10.1d;
-
-decimal d3, d4;
-
-d3 = 10.0m;
-d4 = d3 + 1.0m;
-
-string s, s2;
-
-s = "Hello Worl";
-s2 = s + 'd';
-
-Console.WriteLine("Valor de b e b2 " + b + " " + b2);
-Console.WriteLine("valor de c e c2 " + c + " " + c2);
-Console.WriteLine("valor de b3 e b4 " + b3 + " " + b4);
-Console.WriteLine("valor de i e i2 " + i + " " + i2);
-Console.WriteLine("valor de l e l2 " + l + " " + l2);
-Console.WriteLine("valor de f e f2 " + f + " " + f2);
-Console.WriteLine("valor de d e d2 " + d + " " + d2);
-Console.WriteLine("valor de d3 e d4 " + d3 + " " + d4);
